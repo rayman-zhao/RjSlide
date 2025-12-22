f96562d0694e027641a94622e7db6338e4b6d18a
rayman-zhao <zhao_yu@me.com>
2025-12-22 13:38:19 +0800

    Build java class into jar file

--- ci-debug.bat
+++ ci-debug.bat
@@ -1,5 +1,6 @@
 REM swift build
-%JAVA_HOME%\bin\java ^
--cp .build/plugins/outputs/RjSlide/RjSlide/destination/JavaCompilerPlugin/Java ^
--Djava.library.path=.build/debug ^
+
+jar -cf RjSlide.jar -C .build\plugins\outputs\rjslide\RjSlide\destination\JavaCompilerPlugin\Java\ dev\
+
+%JAVA_HOME%\bin\java -cp .\RjSlide.jar -Djava.library.path=.build/debug ^
 "dev.swiftworks.ruslan.Slide" "C:\Users\zhaoy\Downloads\mushroom.svs"

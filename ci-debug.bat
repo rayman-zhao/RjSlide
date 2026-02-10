31fb48f39a96ec9da55eb1e14682047cc0f474fd
rayman-zhao <zhao_yu@me.com>
2026-02-10 14:11:12 +0800

    Added register test

--- ci-debug.bat
+++ ci-debug.bat
@@ -2,4 +2,5 @@ REM swift build
 
 jar -cf RjSlide.jar -C .build\plugins\outputs\rjslide\RjSlide\destination\JavaCompilerPlugin\Java\ dev\
 
-java -cp .\RjSlide.jar -Djava.library.path=.build/debug "dev.swiftworks.ruslan.Slide" "C:\Users\zhaoy\Downloads\mushroom.svs"
+REM java -cp .\RjSlide.jar -Djava.library.path=.build/debug "dev.swiftworks.ruslan.Slide" "C:\Users\zhaoy\Downloads\mushroom.svs"
+java -cp .\RjSlide.jar -Djava.library.path=.build/debug "dev.swiftworks.ruslan.Register" "./TestData/HE.jpg" "./TestData/IHC.jpg" "./TestOut/"
